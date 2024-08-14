2b3a85040029e8722c11590b313ef1ecbba9cb28
toshihiro.okada <toshihiro.okada@ad.local>
2024-08-14 14:17:37 +0900

    deploy test

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello,Hello,Hello, World!", "message": "Welcome to The World!!"}
