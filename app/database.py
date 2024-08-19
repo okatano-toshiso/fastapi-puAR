08ea5f5391ec15fa5e3afd738cbf1a6deafc2cba
toshihiro.okada <toshihiro.okada@ad.local>
2024-08-19 13:27:22 +0900

    [modi] refactering 2

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import Session
-from .models import SessionLocal, Base, engine, Reservation, Line_User
-
+from .models import SessionLocal
 
 def get_db() -> Session:
     db = SessionLocal()
