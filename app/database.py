9191e98069534e4ca855f6ce6d91cd669593a945
toshihiro.okada <toshihiro.okada@ad.local>
2024-08-19 14:00:03 +0900

    [rem] database.py

--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
-from sqlalchemy.orm import Session
-from .models import SessionLocal, Base, engine, Reservation, Line_User
+# from sqlalchemy.orm import Session
+# from .models import SessionLocal, Base, engine, Reservation, Line_User
 
 
-def get_db() -> Session:
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db() -> Session:
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
