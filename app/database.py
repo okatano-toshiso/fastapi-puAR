7c9aaa1d786a1a11a85494d117a232e736bee7d2
toshihiro.okada <toshihiro.okada@ad.local>
2024-08-19 14:58:09 +0900

    [modi] refactering 9

--- app/database.py
+++ app/database.py
@@ -1,5 +1,8 @@
 from .models import SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from datetime import date, datetime
+from typing import List, Optional
 
 def get_db() -> Session:
     db = SessionLocal()
@@ -7,3 +10,29 @@ def get_db() -> Session:
         yield db
     finally:
         db.close()
+
+class LineReserveBase(BaseModel):
+    token: str
+    reservation_id: int
+    reservation_date: date
+    check_in: date
+    check_out: date
+    line_id: str
+    status: str
+    count_of_person: int
+    room_type: str
+    option_id: Optional[int] = None
+    created_at: datetime
+    updated_at: datetime
+
+class LineUserBase(BaseModel):
+    token: str
+    line_id: str
+    name: str
+    name_kana: Optional[str] = None
+    phone_number: str
+    age: Optional[int] = None
+    adult: bool = True
+    created_at: datetime
+    updated_at: datetime
+
--- app/main.py
+++ app/main.py
@@ -6,39 +6,17 @@ import os
 from typing import List, Optional
 from datetime import date, datetime
 from .models import SessionLocal, LineReserve, LineUser
-from .database import get_db
+from .database import get_db, LineReserveBase, LineUserBase
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 app = FastAPI()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-class LineReserveBase(BaseModel):
-    token: str
-    reservation_id: int
-    reservation_date: date
-    check_in: date
-    check_out: date
-    line_id: str
-    status: str
-    count_of_person: int
-    room_type: str
-    option_id: Optional[int] = None
-    created_at: datetime
-    updated_at: datetime
 
-class LineUserBase(BaseModel):
-    token: str
-    line_id: str
-    name: str
-    name_kana: Optional[str] = None
-    phone_number: str
-    age: Optional[int] = None
-    adult: bool = True
-    created_at: datetime
-    updated_at: datetime
 class RequestData(BaseModel):
     line_reserves: List[LineReserveBase]
     line_users: List[LineUserBase]
+
 
 @app.get("/")
 async def read_test():
